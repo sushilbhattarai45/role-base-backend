9e91ecec33e9b9704adcc26695ecc868656829dc
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2025-05-05 19:07:51 +0545

    Uploadred to cloudinary and mongo

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { cloudinary } from "../config/cloudinaryConfig";
-
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 export const uploadImage = async (
   req: express.Request,
   res: express.Response
@@ -12,13 +13,35 @@ export const uploadImage = async (
     }
     const uploadResult = await cloudinary.uploader
       .upload(req.file.path, {
-        public_id: "shoes",
+        public_id: "user",
       })
       .catch((error) => {
         console.log(error);
       });
 
     console.log(uploadResult);
-    res.send(uploadResult);
-  } catch (e) {}
+    if (uploadResult) {
+      const updateToDB = await prisma.image.create({
+        data: {
+          imageUrl: uploadResult.url,
+          cloudId: uploadResult.asset_id,
+          uploadedBy: "user",
+        },
+      });
+      if (updateToDB) {
+        res.status(200).send({
+          message: "Image Uploaded Succesfully",
+          status: "success",
+        });
+      }
+    } else {
+      res
+        .status(500)
+        .send({ message: "Internal server errors", status: "failed" });
+    }
+  } catch (e) {
+    res
+      .status(500)
+      .send({ message: "Internal server errors", status: "failed" });
+  }
 };
